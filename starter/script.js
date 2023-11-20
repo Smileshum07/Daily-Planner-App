d97a8f0d6dbb361369d5c8766fae3ce464ea461f
Anna <103360175+Smileshum07@users.noreply.github.com>
2023-11-20 20:17:49 +0000

    html/css part and display current date/time

--- starter/script.js
+++ starter/script.js
@@ -1,11 +1,4 @@
 // Vars
 var currentDay = document.getElementById('currentDay');
-//console.log(today);
-var today = new Date();
-var weekDay = today.toDateString().substring(0, 3);
-var date = today.getDate();
-var month = today.toDateString().substring(4, 7);
-var year = today.getFullYear();
-var hours = today.getHours();
-var minutes = today.getMinutes();
-currentDay.textContent = weekDay + ',' + date + ' ' + month + ' ' + year + '      ' + hours + ':' + minutes;
+
+currentDay.textContent = dayjs().format('dddd,  D MMMM  YYYY   h:mm');
