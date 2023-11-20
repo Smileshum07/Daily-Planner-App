9027356b115f5e7d8c95bae2d83cc146f6f11b5c
Anna <103360175+Smileshum07@users.noreply.github.com>
2023-11-20 23:22:07 +0000

    past,present and future not working

--- starter/script.js
+++ starter/script.js
@@ -1,4 +1,39 @@
 // Vars
 var currentDay = document.getElementById('currentDay');
+var currentTime = dayjs().format('HH');
+var hour = document.getElementsByClassName('hour');
+var timeBlock = document.getElementsByClassName('time-block');
+console.log(hour);
+// console.log(timeBlock);
+console.log(currentTime);
+// consol.log(hour);
+// Display current date and time
+currentDay.textContent = dayjs().format('dddd,  D MMMM  YYYY');
 
-currentDay.textContent = dayjs().format('dddd,  D MMMM  YYYY   h:mm');
+
+// Define the past, present and future
+function getRightTime() {
+    hour.forEach(element => {
+        
+        if (element.innerText === currentTime) {
+            timeBlock[i].setAttribute('class', 'present');
+        } else if (element.innerText < currentTime) {
+            timeBlock[i].setAttribute('class', 'past');
+        } else {
+            timeBlock[i].setAttribute('class', 'future');
+        }; 
+    });
+    // for (var i = 0; i < timeBlock.length; i++) {
+        
+    //     if (hour[i].innerText === currentTime) {
+    //         timeBlock[i].setAttribute('class', 'present');
+    //     } else
+    //         if (hour[i].innerText > currentTime) {
+    //         timeBlock[i].setAttribute('class', 'past');
+    //     } else {
+    //         timeBlock[i].setAttribute('class', 'future');
+    //     }; 
+    // }
+};
+
+getRightTime(); 
